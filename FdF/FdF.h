b46bc7641ad27ec55ede94f7236f4d9ff92a2e62
Guilhem Sauvaire <glioburd@gmail.com>
2014-11-29 21:41:24 +0100

    Changing to main

--- FdF/FdF.h
+++ FdF/FdF.h
@@ -6,34 +6,34 @@
 /*   By: gsauvair <gsauvair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/26 18:38:56 by gsauvair          #+#    #+#             */
-/*   Updated: 2014/11/26 18:41:27 by gsauvair         ###   ########.fr       */
+/*   Updated: 2014/11/29 15:29:58 by gsauvair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-#include <mlx.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "FdF.h"
+#include <mlx.h>
+#include <fcntl.h>
+#include "./libft/libft.h"
 //#include "get_next_line.h"
-//#include "./Libft/libft.h"
 
-typedef	struct s_point
+typedef	struct	s_point
 {
-	int	x;
-	int	y;
-	int	z;
-}		t_point;
+	int			x;
+	int			y;
+	int			z;
+}				t_point;
 
-typedef struct s_env
+typedef struct	s_env
 {
-	void *mlx;
-	void *win;
-	int	height;
-	int	width;
-}		t_env;
+	void		*mlx;
+	void		*win;
+	char		*argv;
+}				t_env;
+
+void		ft_read(int fd, t_env *e);
 
 #endif
